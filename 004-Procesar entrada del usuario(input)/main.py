f72f8f6994f69e129c1a113cfcefbc237a59c836
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-10-09 22:58:41 -0600

    Conversion de datos

--- 004-Procesar entrada del usuario(input)/main.py
+++ 004-Procesar entrada del usuario(input)/main.py
@@ -10,3 +10,22 @@ resultado = input()
 print(type(resultado))
 print(resultado)
 
+# Conversion de la entrada de datos
+
+# Manera 1
+# print("Calculadora \nIngresa el primer valor:")
+# resultado1 = input()
+# print("Ingresa el segundo valor:")
+# resultado2 = input()
+# print("El resultado de la suma es:")
+# print(int(resultado1) + int(resultado2))
+
+# Manera 2
+print("Calculadora de suma")
+resultado1 = int(input("Ingrese el primer valor: "))
+resultado2 = int(input("Ingrese el segundo valor: "))
+print("El resultado es:", resultado1 + resultado2)
+
+# Ejercicio; ¿Como estuvo tu dia?
+miDia = input("¿Como estuvo tu dia? (Del 1 al 10):")
+print("Mi dia estuvo de", miDia)
