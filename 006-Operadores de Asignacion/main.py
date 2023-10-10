c2c118387d05fb1fd54adc24b77219cd39d8c7cd
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-10-10 13:14:38 -0600

    Operadores de Asignacion

---
+++ 006-Operadores de Asignacion/main.py
@@ -0,0 +1,29 @@
+
+# Uso de operadores de Asignacion y Reasignacion
+
+# Operador de Asignacion
+miVariable = 10
+print(miVariable)  # 10
+
+# Reasignamos el valor de la variable usando el valor anterior
+miVariable = miVariable + 1
+print(miVariable)  # 11
+
+# Reasignasion de valor
+miVariable += 1
+print(miVariable)  # 12
+
+miVariable -= 2
+print(miVariable)  # 10
+
+miVariable *= 3
+print(miVariable)  # 30
+
+miVariable /= 2
+print(miVariable)  # 15
+
+miVariable //= 2
+print(miVariable)  # 7
+
+miVariable **= 2
+print(miVariable)  # 49
