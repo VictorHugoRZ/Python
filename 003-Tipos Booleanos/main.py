29806ac81d5f306016c8c251238350426c0777b4
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-10-09 22:33:25 -0600

    Manejo de Booleanos

---
+++ 003-Tipos Booleanos/main.py
@@ -0,0 +1,15 @@
+
+# Manejo de Boleanos
+
+miVariable1 = True
+print(miVariable1)  # True
+
+miVariable2 = 3 > 2
+print(miVariable2)  # True
+
+# Evaluamos la expresion de miVariable2 con un bloque If/Else
+
+if miVariable2:  # La expresion es verdadera
+    print("La expresion es verdadera")
+else:
+    print("La expresion es falsa")
