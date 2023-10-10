2626978368b9b0a945c4f9b43d223205fcd8b1a5
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-10-09 22:41:50 -0600

    Manejo de datos en la entrada de la consola

---
+++ 004-Procesar entrada del usuario(input)/main.py
@@ -0,0 +1,12 @@
+
+# Utilizaremos la funcion Input para procesar la entrada de datos del usuario
+
+# Esta funcion nos permite ingresar valores mediante la consola para asignarlos a nuestras variables,
+# estos datos se declararan en nuestras variables con el tipo String, a pesar de ingresar numeros o booleanos
+# en consola
+
+print("Ingresa cualquier valor:")
+resultado = input()
+print(type(resultado))
+print(resultado)
+
