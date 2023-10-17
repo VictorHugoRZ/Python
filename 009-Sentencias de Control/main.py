99c104a03ec7e7372ebf0eeeb16a54d8dfd93029
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-10-17 00:13:59 -0600

    Sentencias de control If/Else

---
+++ 009-Sentencias de Control/main.py
@@ -0,0 +1,11 @@
+
+# Sentencias de control If/Else
+
+condicion = 'Hola'
+
+if condicion == True:
+    print('Condicion valida')
+elif condicion == False:
+    print('Condicion invalida')
+else:
+    print('Condicion no reconocida')
