5df6bc62f7546cc609f01ce5a232c4de0178b903
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-10-18 14:06:10 -0600

    Ejercicio mes y estacion del año

--- 009-Sentencias de Control/main.py
+++ 009-Sentencias de Control/main.py
@@ -43,7 +43,30 @@ else:
 # Condicionado con operador ternario
 print('La condicion es verdadera') if condicion else print('La condicion es falsa')
 
-# Con el operador ternario NO podemos hacer uno de la senteencia ELIF, para esto debemos usar los
+# Con el operador ternario NO podemos hacer uso de la sentencia ELIF, para esto debemos usar los
 # bloques de codigo If/Elif/Else como comunmente los conocemos
+
+'''
+Ejercicio Mes del año:
+El usuario proporcionara el numero de mes en el que esta para saber en que estacion del año se encuentra, 
+esto proporcionando un numero del 1 al 12.
+'''
+
+print('Mes del año')
+mes = int(input('¿En que mes del año te encuentras? (1-12) '))
+estacion = None  # None es el equivalente a Null en otros lenguajes de programacion
+
+if mes == 1 or mes == 2 or mes == 12:
+    estacion = 'Invierno'
+elif mes == 3 or mes == 4 or mes == 5:
+    estacion = 'Primavera'
+elif mes == 6 or mes == 7 or mes == 8:
+    estacion = 'Verano'
+elif mes == 9 or mes == 10 or mes == 11:
+    estacion = 'Otoño'
+else:
+    estacion = 'Mes Invalido'
+
+print(f'Para el mes {mes}, la estacion correspondiente es: {estacion}')
 
 
